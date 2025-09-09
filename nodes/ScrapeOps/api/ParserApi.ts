7c5b4e05f35d8281568af3a5c9900ca7a0622e4b
xorostar <aswadali.me@gmail.com>
2025-09-09 15:10:39 +0500

    refactor: remove unused page types

--- nodes/ScrapeOps/api/ParserApi.ts
+++ nodes/ScrapeOps/api/ParserApi.ts
@@ -30,100 +30,33 @@ export class ParserApi {
         description: 'Domain to parse or select Custom Domain for custom parsing',
       },
       {
-        displayName: 'Page Type',
-        name: 'parserPageType',
-        type: 'options',
+        displayName: 'URL',
+        name: 'parserUrl',
+        type: 'string',
         displayOptions: {
           show: {
             apiType: ['parserApi'],
-            parserDomain: ['amazon'],
           },
         },
-        options: [
-          { name: 'Product Page', value: 'product' },
-          { name: 'Reviews Page', value: 'reviews' },
-          { name: 'Search Page', value: 'search' },
-        ],
-        default: 'product',
-        description: 'Type of Amazon page to parse',
+        default: '',
+        required: true,
+        description: 'URL of the page being parsed',
       },
       {
-        displayName: 'Page Type',
-        name: 'ebayParserPageType',
-        type: 'options',
+        displayName: 'HTML Content',
+        name: 'parserHtml',
+        type: 'string',
+        typeOptions: {
+          rows: 8,
+        },
         displayOptions: {
           show: {
             apiType: ['parserApi'],
-            parserDomain: ['ebay'],
           },
         },
-        options: [
-          { name: 'Category Page', value: 'category' },
-          { name: 'Feedback Page', value: 'feedback' },
-          { name: 'Product Page', value: 'product' },
-          { name: 'Search Page', value: 'search' },
-          { name: 'Store Page', value: 'store' },
-        ],
-        default: 'product',
-        description: 'Type of eBay page to parse',
-      },
-      {
-        displayName: 'Page Type',
-        name: 'walmartParserPageType',
-        type: 'options',
-        displayOptions: {
-          show: {
-            apiType: ['parserApi'],
-            parserDomain: ['walmart'],
-          },
-        },
-
-        options: [
-          { name: 'Browse Page', value: 'browse' },
-          { name: 'Category Page', value: 'category' },
-          { name: 'Product Page', value: 'product' },
-          { name: 'Reviews Page', value: 'reviews' },
-          { name: 'Search Page', value: 'search' },
-          { name: 'Shop Page', value: 'shop' },
-        ],
-        default: 'product',
-        description: 'Type of Walmart page to parse',
-      },
-      {
-        displayName: 'Page Type',
-        name: 'indeedParserPageType',
-        type: 'options',
-        displayOptions: {
-          show: {
-            apiType: ['parserApi'],
-            parserDomain: ['indeed'],
-          },
-        },
-        options: [
-          { name: 'Company Page', value: 'company' },
-          { name: 'Job Detail Page', value: 'job' },
-          { name: 'Search Page', value: 'search' },
-        ],
-        default: 'job',
-        description: 'Type of Indeed page to parse',
-      },
-      {
-        displayName: 'Page Type',
-        name: 'redfinParserPageType',
-        type: 'options',
-        displayOptions: {
-          show: {
-            apiType: ['parserApi'],
-            parserDomain: ['redfin'],
-          },
-        },
-        options: [
-          { name: 'Agent Page', value: 'agent' },
-          { name: 'Property Page', value: 'property' },
-          { name: 'Search Page', value: 'search' },
-        ],
-        default: 'property',
-        description: 'Type of Redfin page to parse',
+        default: '',
+        required: true,
+        description: 'HTML content to parse',
       },
       {
         displayName: 'URL',
